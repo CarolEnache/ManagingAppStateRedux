e049d7bb7285c927503c2ce12df346811dedcc17
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-03 18:19:54 +0100

    bindAction creators and make use of  mapDispatchToProps

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
     renderList(){
@@ -24,5 +26,14 @@ function mapStateToProps(state) {
     books: state.books
   };
 }
-
-export default connect(mapStateToProps)(BookList);
+// Anything returned  from this sunction will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed
+  // to all of our reducers
+  return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selectBook. Make it availeble 
+// as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
