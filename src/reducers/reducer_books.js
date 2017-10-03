bbee2275b27ba613e4eb718738e68c83c3dea523
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-03 17:57:48 +0100

    Create the book-list container and connect it ro react-redux. Call mapStateToProps

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title:'JavaScript'},
+        {title:'Herry Potter'},
+        {title:'The Dark Ruby'},
+        {title:'Eloquent Ruby'}
+    ]
+}
